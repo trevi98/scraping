91237f93790bb62d9b8ef5f9dbbbd6b15592c913
abdullatef trefi <abdullatef.trefi98@gmail.com>
2023-01-17 16:31:24 +0400

    allsop offplan

--- allsopp/allsopp/spiders/offPlan_apartment_spider.py
+++ allsopp/allsopp/spiders/offPlan_apartment_spider.py
@@ -2,49 +2,53 @@ import scrapy
 from bs4 import BeautifulSoup
 from ..items import ApartmentOffplanItem
 from .helpers import methods
-from .file_downloader import img_downloader
+from scrapy.http import FormRequest
+import json
+
+# from .file_downloader import img_downloader
 import uuid
 class AllsoppspiderSpider(scrapy.Spider):
     name = 'apartment_offplan_item'
-    start_urls = ['https://www.allsoppandallsopp.com/dubai/buyers/off-plan']
+    start_urls = ['https://api.allsoppandallsopp.com/dubai/buyers/off-plan']
     page_number=2
     link=""
 
-    def parse(self, response):
-        
-        all= response.css(" a.new_dev_card::attr('href')").extract()  
-        for one in all:
-            self.link=one
-            yield response.follow('https://www.allsoppandallsopp.com/'+one,callback=self.page)
-           
-            
-        next_page = f"https://www.allsoppandallsopp.com/dubai/buyers/off-plan?page={self.page_number}/"
-        if next_page is not None and self.page_number < 1:
-            self.page_number +=1
-            yield response.follow(next_page,callback = self.parse)
-        else:
-            # pass
-            data = {'message': 'driven offplan apartment done'}
+    def start_requests(self):
+    # page = 1    
+        # for page in range(1,2):
+        yield FormRequest(url=self.start_urls[0], callback=self.parse, method='GET', formdata={},headers={"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0) Gecko/20100101 Firefox/109.0","Accept": "*/*","Accept-Language": "en-US,en;q=0.5","Accept-Encoding": "gzip, deflate, br","Referer": "https://opr.ae/","Origin": "https://opr.ae","Connection": "keep-alive","Sec-Fetch-Dest": "empty","Sec-Fetch-Mode": "no-cors","Sec-Fetch-Site": "same-site","TE": "trailers","Content-Type": "application/x-www-form-urlencoded; charset=UTF-8","Pragma": "no-cache","Cache-Control": "no-cache"})
+        # else:
+            # data = {'message': 'opr project done'}
             # response = requests.post("https://notifier.abdullatif-treifi.com/", data=data)
-            # sys.path.append('/c/Python310/Scripts/scrapy')
-    def page(self ,response):
+
+
+    def parse(self, response):
         items = ApartmentOffplanItem()
         signature = uuid.uuid1()
-        # title=response.css(".container .last_content div ")
-        #price
-        items["area"]=response.css(".offplan_detail_properties .container .first_content span::text").get()
-        items["description"]=response.css(".offplan_detail_properties .container .first_content p::text").get() 
-        items["developer"]=response.css(".offplan_detail_properties .container .last_content div")[1].css("div p::text").get()
-        items["handover_date"]=response.css(".offplan_detail_properties .container .last_content div")[2].css("div p::text").get()
-        items['images']=methods.img_downloader_method_src(response.css(".slick-list").get(),signature)
-        all_Proximity=[]
-        all_palces=  response.css("section.places_in_proximity.container .main_content .places .place_item .place_name") 
-        for one_place in all_palces:
-            all_Proximity.append(one_place.css("span::text").get())
-        items["all_proximity"]=all_Proximity
-        all_Payments=[]
-        all_payments_plan=response.css("div.payment_plan.container .payment_plan_content .main_content .detail_items .item")
-        for one_payment in all_payments_plan:
-            all_Payments.append({one_payment.css("div b::text").get().replace("<!-- -->",""):one_payment.css("div::text")[0].get()})
-        items["all_payment"]=all_Payments
-        yield items          
+        res = json.loads(response.text)
+        for project in res['data']['data']:
+            items['title'] = project['attributes']['name']
+            items['area'] = project['attributes']['area']
+            items['priceFrom'] = project['attributes']['priceFrom']
+            items['priceTo'] = project['attributes']['priceTo']
+            items['description'] = project['attributes']['description'].replace("\n","")
+            items['handoverDate'] = project['attributes']['handoverDate'].replace("\n","")
+            try:
+                items['location'] = project['attributes']['location'].replace("\n","")
+            except:
+                items['location'] = "N\A"
+            items['featuredImage'] = methods.img_downloader_method_from_list([project['attributes']['featuredImage']['data']['attributes']['url']],signature)
+            try:
+                items['brochure'] = project['attributes']['Brochure']['data']['url']
+            except:
+                items['brochure'] = "N\A"
+            items['property_developers'] = project['attributes']['property_developers']['data'][0]['attributes']['name'].replace("\n","")
+            floor_plans = []
+            try:
+                for i in project['attributes']['floorPlan']['data']:
+                    floor_plans.append(i['attributes']['url'])
+            except:
+                floor_plans = []
+            items['floor_plans'] = methods.img_downloader_method_from_list(floor_plans,signature)
+            yield items
+     
