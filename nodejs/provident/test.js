c89cb4aed6777608112ed88635eaac0a826239ac
hassanballeh <hassanballeh@gmail.com>
2023-02-08 16:41:53 +0300

    ss

---
+++ nodejs/provident/test.js
@@ -0,0 +1,21 @@
+const puppeteer = require("puppeteer");
+async function run() {
+  const browser = await puppeteer.launch({
+    headless: true,
+    executablePath:
+      "C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe",
+    args: ["--enable-automation"],
+  });
+  const page = await browser.newPage();
+  await page.goto(
+    "https://www.providentestate.com/dubai-off-plan-properties.html"
+  );
+  const links = await page.evaluate(() => {
+    let as = Array.from(document.querySelectorAll(".item-post__image"));
+    let uniqe = [...new Set(as)];
+    return uniqe;
+  });
+  console.log(links);
+  await browser.close();
+}
+run();
