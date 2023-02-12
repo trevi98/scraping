bc5e26710670479b31b1b5e435167e885f65f995
hassanballeh <hassanballeh@gmail.com>
2023-02-12 19:50:59 +0300

    opr

--- nodejs/opr/one.js
+++ nodejs/opr/one.js
@@ -11,23 +11,53 @@ async function run() {
   await page.goto(
     "https://opr.ae/projects/emaar-mina-rashid-yacht-marina-seascape-apartments-in-dubai"
   );
-  const links = await page.evaluate(() => {
-    let aaa = [];
+  const floor = await page.evaluate(() => {
     // .node.section-clear.section div.container.fullwidth div.cont div.node.widget-tabs.cr-tabs.widget div.metahtml div.tabs1-container.swiper-container.swiper-container-horizontal.swiper-container-autoheight div.tabs1-root div.swiper-wrapper div.swiper-slide
-    let floor_plans = Array.from(
-      document.querySelectorAll("#fp .swiper-slide p a .fr-dib.fr-draggable")
-    );
-    floor_plans.forEach((e) => {
-      aaa.push(e.src);
-    });
-
-    return {
-      floor_plans: aaa,
-      s: floor_plans.length,
-    };
+    let number = Array.from(document.querySelectorAll("#fp .tabs1-page"));
+    let f = [];
+    for (let i = 0; i < number.length; i++) {
+      number[i].id = `h${i + 1}`;
+      f.push(`h${i + 1}`);
+    }
+    return f;
   });
-  console.log(links);
+  // console.log(links.length);
+  let floor_plans = [];
+  if (floor.length > 0) {
+    for (let f of floor) {
+      await page.click(`#${f}`);
+      console.log(f);
+      floor_plans.push(
+        await page.evaluate(() => {
+          let size = "";
+          let img = "";
+          let title = "";
+          try {
+            size = document.querySelector(
+              "#fp .swiper-slide.swiper-slide-active .node.widget-text.cr-text.widget + .node.widget-text.cr-text.widget p"
+            ).textContent;
+          } catch (error) {}
 
+          try {
+            img = document.querySelector(
+              "#fp .swiper-slide.swiper-slide-active a .fr-dib.fr-draggable"
+            ).src;
+          } catch (error) {}
+          try {
+            title = document.querySelector(
+              "#fp .swiper-slide.swiper-slide-active h3"
+            ).textContent;
+          } catch (error) {}
+          return {
+            title: title,
+            size: size,
+            img: img,
+          };
+        })
+      );
+    }
+  }
+  console.log(floor_plans);
   await browser.close();
 }
 run();
--- nodejs/opr/test.js
+++ nodejs/opr/test.js
@@ -12,92 +12,139 @@ async function run() {
     "https://opr.ae/projects/emaar-mina-rashid-yacht-marina-seascape-apartments-in-dubai"
   );
   const links = await page.evaluate(() => {
-    //   let price_payment = [];
-    //   let a = document.querySelectorAll(
-    //     "#header-menu-desktop ~ div.node.section-zero.section div.zero-layer-axis.lg-left.lg-bottom strong"
-    //   );
-    //   a.forEach((e) => price_payment.push(e.textContent));
-    //   let handover = Array.from(
-    //     document.querySelectorAll(
-    //       "#header-menu-desktop ~ div.node.section-zero.section div.zero-layer-axis.lg-center.lg-bottom strong"
-    //     )
-    //   )[0].innerText;
-    //   let overview = document.querySelector(
-    //     "#header-menu-desktop ~ div.node.section-zero.section div.zero-layer-axis.lg-left.lg-middle p"
-    //   ).textContent;
-    //   let temp = document.querySelector(
-    //     "#header-menu-desktop ~ div.node.section-zero.section div.zero-layer-axis.lg-left.lg-top strong"
-    //   ).textContent;
-    //   let temp2 = "";
-    //   try {
-    //     temp2 = document.querySelector(
-    //       "#header-menu-desktop ~ div.node.section-zero.section div.zero-layer-axis.lg-left.lg-middle strong"
-    //     ).textContent;
-    //   } catch (error) {
-    //     temp2 = Array.from(
-    //       document.querySelectorAll(
-    //         "#header-menu-desktop ~ div.node.section-zero.section div.zero-layer-axis.lg-left.lg-top strong"
-    //       )
-    //     )[1].innerText;
-    //   }
-    //   let title = temp + " " + temp2;
-    //   let about = document.querySelector(
-    //     "div#about.node.section-clear.section div.node.widget-grid.widget.xs-hidden div.col div.node.widget-text.cr-text.widget.xs-hidden.links-on-black-text p.textable"
-    //   ).textContent;
-    //   let proprty_info = [];
-    //   temp = document.querySelectorAll(
-    //     "div#about.node.section-clear.section div.node.widget-grid.widget.xs-hidden div.grid.valign-middle.paddings-40px.xs-wrap div.gridwrap div.col div.cont div.node.widget-element.widget div.cont div.node.widget-text.cr-text.widget p.textable"
-    //   );
-    //   temp.forEach((e) => {
-    //     proprty_info.push(e.textContent);
-    //   });
-    //   let location = document.querySelector(
-    //     "div#location.node.section-clear.section div.container.fullwidth div.cont div.node.widget-grid.widget div.grid.valign-top.paddings-40px.xs-wrap div.gridwrap div.col div.cont div.node.widget-text.cr-text.widget.links-on-black-text p.textable"
-    //   ).textContent;
-    //   let nearby_place = [];
-    //   temp = Array.from(
-    //     document.querySelectorAll(
-    //       "div#location.node.section-clear.section div.node.widget-grid.widget.xs-hidden div.grid.valign-middle.paddings-40px.xs-wrap div.gridwrap div.col div.cont div.node.widget-element.widget div.cont"
-    //     )
-    //   );
-    //   temp.forEach((e) => nearby_place.push(e.textContent));
-    //   payment_plan = [];
-    //   temp = document.querySelectorAll(
-    //     "div#pp.node.section-clear.section div.container div.cont div.node.widget-grid.widget div.grid.valign-middle.paddings-40px.xs-wrap div.gridwrap div.col div.cont div.node.widget-element.widget div.cont"
-    //   );
-    //   temp.forEach((e) => payment_plan.push(e.textContent));
-    //   aaa = [];
-    //   floor_plans = Array.from(
-    //     document.querySelectorAll(
-    //       "div#fb.node.section-clear.section div.container.fullwidth div.cont div.node.widget-tabs.cr-tabs.widget div.metahtml div.tabs1-container.swiper-container.swiper-container-horizontal.swiper-container-autoheight div.tabs1-root div.swiper-wrapper div.swiper-slide"
-    //     )
-    //   );
-    //   floor_plans.forEach((e) => aaa.push(e.textContent));
-    //   let images = Array.from(
-    //     document.querySelectorAll(
-    //       ".node.section-clear.section div.container.fullwidth div.cont div.node.widget-tabs.cr-tabs.widget div.metahtml div.tabs1-root div.swiper-wrapper div.swiper-slide .cont-wrapper .gallery1-image.fancybox "
-    //     )
-    //   );
-    //   let all_images = [];
-    //   images.forEach((e) => {
-    //     all_images.push(e.href);
-    //   });
-    //   all_images = [...new Set(all_images)];
-    //   return {
-    //     title: title,
-    //     overview: overview,
-    //     price_payment: price_payment,
-    //     handover: handover,
-    //     about: about,
-    //     proprty_info: proprty_info,
-    //     location: location,
-    //     nearby_place: nearby_place,
-    //     payment_plan: payment_plan,
-    //     floor_plans: floor_plans,
-    //     images: all_images,
-    //   };
+    let price_payment = [];
+    let a = document.querySelectorAll(
+      "#header-menu-desktop ~ div.node.section-zero.section div.zero-layer-axis.lg-left.lg-bottom strong"
+    );
+    a.forEach((e) => price_payment.push(e.textContent));
+    let handover = Array.from(
+      document.querySelectorAll(
+        "#header-menu-desktop ~ div.node.section-zero.section div.zero-layer-axis.lg-center.lg-bottom strong"
+      )
+    )[0].innerText;
+    let overview = document.querySelector(
+      "#header-menu-desktop ~ div.node.section-zero.section div.zero-layer-axis.lg-left.lg-middle p"
+    ).textContent;
+    let temp = document.querySelector(
+      "#header-menu-desktop ~ div.node.section-zero.section div.zero-layer-axis.lg-left.lg-top strong"
+    ).textContent;
+    let temp2 = "";
+    try {
+      temp2 = document.querySelector(
+        "#header-menu-desktop ~ div.node.section-zero.section div.zero-layer-axis.lg-left.lg-middle strong"
+      ).textContent;
+    } catch (error) {
+      temp2 = Array.from(
+        document.querySelectorAll(
+          "#header-menu-desktop ~ div.node.section-zero.section div.zero-layer-axis.lg-left.lg-top strong"
+        )
+      )[1].innerText;
+    }
+    let title = temp + " " + temp2;
+    let about = document.querySelector(
+      "div#about.node.section-clear.section div.node.widget-grid.widget.xs-hidden div.col div.node.widget-text.cr-text.widget.xs-hidden.links-on-black-text p.textable"
+    ).textContent;
+    let proprty_info = [];
+    temp = document.querySelectorAll(
+      "div#about.node.section-clear.section div.node.widget-grid.widget.xs-hidden div.grid.valign-middle.paddings-40px.xs-wrap div.gridwrap div.col div.cont div.node.widget-element.widget div.cont div.node.widget-text.cr-text.widget p.textable"
+    );
+    temp.forEach((e) => {
+      proprty_info.push(e.textContent);
+    });
+    let location = document.querySelector(
+      "div#location.node.section-clear.section div.container.fullwidth div.cont div.node.widget-grid.widget div.grid.valign-top.paddings-40px.xs-wrap div.gridwrap div.col div.cont div.node.widget-text.cr-text.widget.links-on-black-text p.textable"
+    ).textContent;
+    let nearby_place = [];
+    temp = Array.from(
+      document.querySelectorAll(
+        "div#location.node.section-clear.section div.node.widget-grid.widget.xs-hidden div.grid.valign-middle.paddings-40px.xs-wrap div.gridwrap div.col div.cont div.node.widget-element.widget div.cont"
+      )
+    );
+    temp.forEach((e) => nearby_place.push(e.textContent));
+    payment_plan = [];
+    temp = document.querySelectorAll(
+      "div#pp.node.section-clear.section div.container div.cont div.node.widget-grid.widget div.grid.valign-middle.paddings-40px.xs-wrap div.gridwrap div.col div.cont div.node.widget-element.widget div.cont"
+    );
+    temp.forEach((e) => payment_plan.push(e.textContent));
+    aaa = [];
+    floor_plans = Array.from(
+      document.querySelectorAll(
+        "div#fb.node.section-clear.section div.container.fullwidth div.cont div.node.widget-tabs.cr-tabs.widget div.metahtml div.tabs1-container.swiper-container.swiper-container-horizontal.swiper-container-autoheight div.tabs1-root div.swiper-wrapper div.swiper-slide"
+      )
+    );
+    floor_plans.forEach((e) => aaa.push(e.textContent));
+    let images = Array.from(
+      document.querySelectorAll(
+        ".node.section-clear.section div.container.fullwidth div.cont div.node.widget-tabs.cr-tabs.widget div.metahtml div.tabs1-root div.swiper-wrapper div.swiper-slide .cont-wrapper .gallery1-image.fancybox "
+      )
+    );
+    let all_images = [];
+    images.forEach((e) => {
+      all_images.push(e.href);
+    });
+    all_images = [...new Set(all_images)];
+
+    return {
+      title: title,
+      overview: overview,
+      price_payment: price_payment,
+      handover: handover,
+      about: about,
+      proprty_info: proprty_info,
+      location: location,
+      nearby_place: nearby_place,
+      payment_plan: payment_plan,
+      floor_plans: floor_plans,
+      images: all_images,
+    };
   });
-  // console.log(links);
+  console.log(links);
+  const floor = await page.evaluate(() => {
+    // .node.section-clear.section div.container.fullwidth div.cont div.node.widget-tabs.cr-tabs.widget div.metahtml div.tabs1-container.swiper-container.swiper-container-horizontal.swiper-container-autoheight div.tabs1-root div.swiper-wrapper div.swiper-slide
+    let number = Array.from(document.querySelectorAll("#fp .tabs1-page"));
+    let f = [];
+    for (let i = 0; i < number.length; i++) {
+      number[i].id = `h${i + 1}`;
+      f.push(`h${i + 1}`);
+    }
+    return f;
+  });
+  let floor_plans = [];
+  if (floor.length > 0) {
+    for (let f of floor) {
+      await page.click(`#${f}`);
+      console.log(f);
+      floor_plans.push(
+        await page.evaluate(() => {
+          let size = "";
+          let img = "";
+          let title = "";
+          try {
+            size = document.querySelector(
+              "#fp .swiper-slide.swiper-slide-active .node.widget-text.cr-text.widget + .node.widget-text.cr-text.widget p"
+            ).textContent;
+          } catch (error) {}
+
+          try {
+            img = document.querySelector(
+              "#fp .swiper-slide.swiper-slide-active a .fr-dib.fr-draggable"
+            ).src;
+          } catch (error) {}
+          try {
+            title = document.querySelector(
+              "#fp .swiper-slide.swiper-slide-active h3"
+            ).textContent;
+          } catch (error) {}
+          return {
+            title: title,
+            size: size,
+            img: img,
+          };
+        })
+      );
+    }
+  }
+  console.log(floor_plans);
 
   //############### brochure #####################
   const exists = await page.evaluate(() => {
@@ -135,16 +182,20 @@ async function run() {
     console.log("yes");
     // data.push({brochure:url})
     console.log(brochure);
-    await page.goBack();
+    await page.goto(
+      "https://opr.ae/projects/emaar-mina-rashid-yacht-marina-seascape-apartments-in-dubai"
+    );
   } else {
     console.log("yyyy");
   }
 
   // floor###########
-  let n = "";
+
   const exists_plan_btn = await page.evaluate(() => {
-    document.querySelector(
-      "#fp .node.widget-element.widget .cont .node.widget-button.widget.lg-hidden .button-container.left.xs-full .button-wrapper a"
+    return (
+      document.querySelector(
+        "#fp .node.widget-element.widget .cont .node.widget-button.widget.lg-hidden .button-container.left.xs-full .button-wrapper a"
+      ) !== null
     );
   });
   if (exists_plan_btn) {
@@ -183,3 +234,15 @@ async function run() {
   await browser.close();
 }
 run();
+// let array = Array.from(
+//   document.querySelectorAll(
+//     "#fp .node.widget-element.widget .cont .node.widget-button.widget.lg-hidden .button-container.left.xs-full .button-wrapper a"
+//   )
+// );
+// array.forEach((e) => {
+//   if (/floor/i.test(e.querySelector("span").textContent)) {
+//     n = e;
+//     return true;
+//   }
+// });
+// return false;
